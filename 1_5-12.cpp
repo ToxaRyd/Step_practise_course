f9d5b31f5de0a3069d75eacda210b8326d390915
ToxaRyd <65728417+ToxaRyd@users.noreply.github.com>
2021-01-06 19:57:02 +0200

    Add files via upload

--- 1_5-12.cpp
+++ 1_5-12.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main ()
-{
-    int now, a = 1, b = 0, index = 0;
-    cin >> now;
-    while (now != b)
-    {
-        a = a + b;
-        b = a - b;
-        ++index;
-        if (now < b)
-        {
-            index = -1;
-            break;
-        }
-    }
-    cout << index;
+int main() {
+	int n = 0, x = 0, y = 1, c = 1;
+	cin >> n;
+
+	while (n !=0) {
+		if (n == x) {
+			c += 1;
+		}
+		if (n != x) {
+		    if (c > y) {y = c; c = 1;}
+		    else {c = 1;}
+		}
+		x = n;
+		cin >> n;
+	}
+	if (y > c) {cout << y;}
+	else {cout << c;}
+	return 0;
 }
